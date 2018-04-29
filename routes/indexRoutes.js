36c2a774c541c1238a9589dea076900cc4bf4a3a
Amit <amitr1983@github.com>
2018-04-29 00:52:25 -0700

    Changing homepage

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -2,8 +2,9 @@
 module.exports = function(app) {
   var indexPage = require('../controllers/indexController');
 
- app.route('/')
-    .get(indexPage.show_home)
+ 	app.get('/', function (req, res) {
+        res.render('viewSearchResult.ejs');
+    });
     
     app.get('/view', function (req, res) {
         res.render('viewSearchResult.ejs');
