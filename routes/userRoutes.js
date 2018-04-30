05e154b74173b2fc27dba6e01ed0c782e297a3d7
mojdeh <mojdehkeykhanzadeh@Mojdehs-MacBook-Air.local>
2018-04-29 20:40:43 -0700

    added environment variables for cognito

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,13 +5,18 @@ const passport = require('passport');
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
+
+
 module.exports = function (app) {
+    var redirect_uri=process.env.COGNITO_REDIRECT_URI;
+    var client_id = process.env.GOGNITO_CLIENT_ID;
+
     app.get('/signup', function (req, res) {
         res.send({ message: 'Please create a new account using post request to /signup and pass email & password params' });
     });
 
     app.get('/signin', function (req, res) {
-        res.redirect("https://ucompare.auth.us-east-1.amazoncognito.com/login?redirect_uri=https://localhost:3000/&response_type=code&client_id=xxxxxxxxxxxxxxxx")
+        res.redirect("https://ucompare.auth.us-east-1.amazoncognito.com/login?redirect_uri=redirect_uri&response_type=code&client_id=client_id")
     });
 
     app.post('/signin', requireSignin, Authentication.signin);
