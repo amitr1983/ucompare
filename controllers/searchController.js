70aa54a0e5c70f3c48ea85e872dd9e493a4bfcc3
Amit <amitr1983@github.com>
2018-05-10 23:35:21 -0700

    read keys from mongo

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,28 +1,48 @@
 // Controller for Search page
+var MongoClient = require('mongodb').MongoClient
+  , Server = require('mongodb').Server;
+
 
 exports.searchProduct = function (req, res, next) {
-  var api_key = process.env.SM_API_KEY
-  var api_secret = process.env.SM_API_SECRET
-  var sem3 = require('semantics3-node')(api_key,api_secret);
-  var Semantics3 = require('semantics3-node-client');
 
-  const product = req.query.q;
+  var api_key = ""
+  var api_secret = ""
+
+  MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+  if(err) { return console.dir(err); }
+
+  var collection = db.collection('apiKeys');
+
+    collection.find().toArray(function(err, kittens) {
+      api_key = kittens[0].accessKey
+      api_secret = kittens[0].secretKey
  
-  sem3.products.products_field( "search", product );
-  // sem3.products.products_field( "sort", "price", "asc" );
-  sem3.products.products_field( "activeproductsonly", 1);
-  sem3.products.products_field( "isactive", true);
 
-// Run the request
-  sem3.products.get_products(
-     function(err, products) {
-       if (err) {
-         console.log("Couldn't execute request: get_products");
-         return;
-      }
-    // View results of the request
-    console.log( "Results of request:\n" + JSON.stringify( products ) );
-    res.json(products)
-   }
- );
+        console.log(api_key)
+        console.log(api_secret)
+  
+        var sem3 = require('semantics3-node')(api_key,api_secret);
+        var Semantics3 = require('semantics3-node-client');
+
+        const product = req.query.q;
+       
+        sem3.products.products_field( "search", product );
+        // sem3.products.products_field( "sort", "price", "asc" );
+        sem3.products.products_field( "activeproductsonly", 1);
+        sem3.products.products_field( "isactive", true);
+
+      // Run the request
+        sem3.products.get_products(
+           function(err, products) {
+             if (err) {
+               console.log("Couldn't execute request: get_products");
+               return;
+            }
+          // View results of the request
+          console.log( "Results of request:\n" + JSON.stringify( products ) );
+          res.json(products)
+         }
+       );
+     });    
+  });
 }
